167db1fe561b696e15b377b79ee4dfeedeb2380f
Marcelo Barbosa <marcelo.barbosa@b2.solutions>
2025-01-18 18:15:17 -0300

    first commit

---
+++ ScreenSound/Migrations/20250118174115_projetoInicial.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class projetoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {           
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistasId = 1 WHERE MusicaId = 1");
+            //migrationBuilder.CreateTable(
+            //    name: "Artistas",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(type: "int", nullable: false)
+            //            .Annotation("SqlServer:Identity", "1, 1"),
+            //        Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+            //        FotoPerfil = table.Column<string>(type: "nvarchar(max)", nullable: false),
+            //        Bio = table.Column<string>(type: "nvarchar(max)", nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Artistas", x => x.Id);
+            //    });
+
+            //migrationBuilder.CreateTable(
+            //    name: "Musicas",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(type: "int", nullable: false)
+            //            .Annotation("SqlServer:Identity", "1, 1"),
+            //        Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Musicas", x => x.Id);
+            //    });
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Artistas");
+
+            migrationBuilder.DropTable(
+                name: "Musicas");
+        }
+    }
+}
---
+++ ScreenSound/Migrations/20250118202439_RelacionarArtistaMusica.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelacionarArtistaMusica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AnoLancamento",
+                table: "Musicas",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ArtistaId",
+                table: "Musicas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Musicas_ArtistaId",
+                table: "Musicas",
+                column: "ArtistaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Musicas_Artistas_ArtistaId",
+                table: "Musicas",
+                column: "ArtistaId",
+                principalTable: "Artistas",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Musicas_Artistas_ArtistaId",
+                table: "Musicas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Musicas_ArtistaId",
+                table: "Musicas");
+
+            migrationBuilder.DropColumn(
+                name: "ArtistaId",
+                table: "Musicas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AnoLancamento",
+                table: "Musicas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
